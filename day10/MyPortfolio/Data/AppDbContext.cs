33707081c0520ad57843b1c4147f7d1756cdd0f9
HyungJuu <rmsdk617@naver.com>
2024-07-29 10:21:52 +0900

    빠진부분 추가 수정완

--- day10/MyPortfolio/Data/AppDbContext.cs
+++ day10/MyPortfolio/Data/AppDbContext.cs
@@ -8,10 +8,11 @@ namespace MyPortfolio.Data
         // 생성자
         public AppDbContext(DbContextOptions options) : base(options)
         {
-
         }
-        
+
         // CodeFirst로 테이블로 만들 엔티티 클래스 정의
         public DbSet<Board> Board { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<Project> Project { get; set; }
     }
 }
